6910a3a434534ddb52cc1923a5c3da5bfe74d6cd
Arife Kara <arifedagkara@gmail.com>
2022-12-04 01:17:47 +0300

    sartlı-jsx örnegi tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function Bilesen({KutuNo}) {
+  let sayi = KutuNo
+  return(
+    <>
+    <p>Hangi kutu ?</p>
+    {sayi=== 2 ?<Kutu2/> : <Kutu1/>};
+    </>
+    )
+}
+function Kutu1(){
+  return "Ben kutu 1'im "
+}
+function Kutu2() {
+  return "Ben kutu 2'yim"
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <h1>Merhaba dünya!</h1>
+  <Bilesen KutuNo={2}></Bilesen>
 );
-
-
